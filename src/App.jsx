72a18d0a025a87e714370b73f1f3c697fecb0125
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-02-24 13:46:16 -0500

    Nav changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header"
 import Nav from "./components/Nav"
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Nav />
+      <Header />
     </>
   )
 }
