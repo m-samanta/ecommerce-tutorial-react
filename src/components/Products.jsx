790efb3dca0021c230ed1d2ba8b7636be7e2b760
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-02-24 15:24:05 -0500

    Product Skeleton

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Product from "./ui/Product";
 import axios from "axios";
+import ProductSkeleton from "./ui/ProductSkeleton";
 
 const Products = () => {
-  const [ products, setProducts ] = useState([]);
+  const [products, setProducts] = useState([]);
   async function fetchProducts() {
     const { data } = await axios.get(
       "https://ecommerce-samurai.up.railway.app/product"
@@ -24,9 +25,15 @@ const Products = () => {
         <div className="row products__row">
           <h2 className="products__title">Products we are proud of</h2>
           <div className="products__list">
-            {products.slice(0, 8).map((product) => (
-              <Product key={product.id} product={product}/>
-            ))}
+            {products.length > 0 ? (
+              products
+                .slice(0, 8)
+                .map((product) => (
+                  <Product key={product.id} product={product} />
+                ))
+            ) : (
+              new Array(8).fill(0).map(() => <ProductSkeleton />)
+            )}
           </div>
         </div>
       </div>
