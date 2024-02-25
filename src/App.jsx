9cd412b54083cbe596641afb4e477e2057716a69
Mriganka Samanta <m.samanta.dev@gmail.com>
2024-02-24 19:55:50 -0500

    HomePage sorted

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import axios from "axios";
 import Banner2 from "./components/Banner2";
 import Newsletter from "./components/Newsletter";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import HomePage from "./Pages/HomePage";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -28,14 +30,15 @@ function App() {
 
   return (
     <AppContext.Provider value= {{ products }}>
-      <Nav />
-      <Header />
-      <Products />
-      <Banner1 />
-      <TrendingProducts />
-      <Banner2 />
+     <Router>
+     <Nav />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
       <Newsletter />
       <Footer />
+      
+     </Router>
     </AppContext.Provider>
   )
 }
